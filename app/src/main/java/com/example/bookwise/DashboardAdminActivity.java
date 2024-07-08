dce1caca482f5c2b89ceb07fe433ee2999f694ab
dzaki alif <alhidayat2404@gmail.com>
2024-07-09 03:33:34 +0700

    part 4

--- app/src/main/java/com/example/bookwise/DashboardAdminActivity.java
+++ app/src/main/java/com/example/bookwise/DashboardAdminActivity.java
@@ -1,14 +1,25 @@
 package com.example.bookwise;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Display;
 import android.view.View;
 
 import com.example.bookwise.databinding.ActivityDashboardAdminBinding;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class DashboardAdminActivity extends AppCompatActivity {
 
@@ -17,6 +28,12 @@ public class DashboardAdminActivity extends AppCompatActivity {
 
     //firebase auth
     private FirebaseAuth firebaseAuth;
+
+    //arrayList to store category
+    private ArrayList<ModelCategory> categoryArrayList;
+
+    //adapter
+    private AdapterCategory adapterCategory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +44,30 @@ public class DashboardAdminActivity extends AppCompatActivity {
         //init firebase auth
         firebaseAuth = FirebaseAuth.getInstance();
         checkUser();
+        loadCategories();
+
+        //edit text change listern, search
+        binding.searchEt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                //called as and when user type each letter
+                try {
+                    adapterCategory.getFilter().filter(s);
+                }catch (Exception e){
+
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
         //handle click logout
         binding.logoutBtn.setOnClickListener(new View.OnClickListener() {
@@ -42,6 +83,38 @@ public class DashboardAdminActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(DashboardAdminActivity.this, CategoryAddActivity.class));
+            }
+        });
+    }
+
+    private void loadCategories() {
+        //init arraylist
+        categoryArrayList = new ArrayList<>();
+
+        //get all categories from firebase categories
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("categories");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //clear array list before adding data into it
+                categoryArrayList.clear();
+                for (DataSnapshot ds:snapshot.getChildren()){
+                    //get data
+                    ModelCategory model = ds.getValue(ModelCategory.class);
+
+                    //add to array
+                    categoryArrayList.add(model);
+                }
+
+                //setup adapter
+                adapterCategory = new AdapterCategory(DashboardAdminActivity.this, categoryArrayList);
+                //set adapter to recyclerview
+                binding.categoriesRv.setAdapter(adapterCategory);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
     }
---
+++ app/src/main/java/com/example/bookwise/FilterCategory.java
@@ -0,0 +1,57 @@
+package com.example.bookwise;
+
+import android.widget.Filter;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class FilterCategory extends Filter {
+    //array list in which we want to search
+    ArrayList<ModelCategory> filterList;
+    //adapter in which filter need to be implemented
+    AdapterCategory adapterCategory;
+
+    //constructor
+    public FilterCategory(ArrayList<ModelCategory> filterList, AdapterCategory adapterCategory) {
+        this.filterList = filterList;
+        this.adapterCategory = adapterCategory;
+    }
+
+
+    @Override
+    protected FilterResults performFiltering(CharSequence constraint) {
+        FilterResults results = new FilterResults();
+        //value should not be null and empty
+        if (constraint != null && constraint.length() > 0){
+
+            //change to upper case, or lowercase to avoid case sensitivity
+            constraint = constraint.toString().toUpperCase();
+            ArrayList<ModelCategory> filteredModels = new ArrayList<>();
+
+            for (int i = 0; i < filterList.size(); i++){
+                //validate
+                if (filterList.get(i).getCategory().toUpperCase().contains(constraint)){
+                    //add to filtered list
+                    filteredModels.add(filterList.get(i));
+                }
+            }
+            results.count = filteredModels.size();
+            results.values = filteredModels;
+        }
+        else {
+            results.count = filterList.size();
+            results.values = filterList;
+        }
+
+        return results; //dont miss it
+    }
+
+    @Override
+    protected void publishResults(CharSequence constraint, FilterResults results) {
+        //apply filter changes
+        adapterCategory.categoryArrayList = (ArrayList<ModelCategory>) results.values;
+
+        //notify changes
+        adapterCategory.notifyDataSetChanged();
+    }
+}
